59c2494d3c549f11d9c140098f29a860875e54b7
MD ALISHAN ALI <md.alishanali88@gmail.com>
2023-05-31 23:17:27 +0530

    added example of class and functinal component

---
+++ react/1.components/Functional.tsx
@@ -0,0 +1,12 @@
+// Function Component
+import React from 'react';
+
+interface FunctionComponentProps {
+  name: string;
+}
+
+const FunctionComponent: React.FC<FunctionComponentProps> = ({ name }) => {
+  return <h1>Hello, {name}!</h1>;
+};
+
+export default FunctionComponent;
